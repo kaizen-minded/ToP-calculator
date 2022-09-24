122e0cf619186994aa318c4c5430fff66a3d3aaf
Melvin <melvin3ezell@gmail.com>
2022-09-24 13:16:24 -0400

    updated if calculate statement

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ calculator.addEventListener("click", (e) => {
   if (dataType === "number") {
     if (calculatorState.previousKeyType === "operator") {
       calculatorState.firstOperand = calculatorState.displayValue;
-      calculatorState.displayValue = "0";
+      resetStateValues("displayValue");
+      // calculatorState.displayValue = "0";
     }
     updateDisplayValue(e.target.innerText);
     display.innerText = calculatorState.displayValue;
@@ -29,40 +30,24 @@ calculator.addEventListener("click", (e) => {
       calculatorState.firstOperand
     ) {
       // debugger;
-      calculatorState.displayValue = operate(
-        calculatorState.operator,
-        calculatorState.firstOperand,
-        calculatorState.displayValue
-      );
+      calculatorState.displayValue = operate();
       display.innerText = calculatorState.displayValue;
     }
     calculatorState.operator = e.target.innerText;
     calculatorState.previousKeyType = "operator";
   }
   // This whole if statement could just be a function
-  if (dataType === "calculate") {
-    // Is there ever a time when calculatorState.displayValue is not true?
-    // If it is always true just delete this
-    if (calculatorState.displayValue && calculatorState.firstOperand) {
-      // Running the operate function then storing the value in calculatorState.displayValue
-      // Looks like I can use deconstruction here
-      calculatorState.displayValue = operate(
-        calculatorState.operator,
-        calculatorState.firstOperand,
-        calculatorState.displayValue
-      );
-      // I'm turning the values back to their original settings
-      // Maybe I can do a function that takes unknown amount of arguements
-
-      // calculatorState.operator = null;
-      // calculatorState.firstOperand = null;
-      resetStateValues("operator", "firstOperand");
-      // updateDisplayUI needs to be its own function
-      display.innerText = calculatorState.displayValue;
-    }
+  if (dataType === "calculate" && calculatorState.firstOperand) {
+    setCalculateStateValue("displayValue", operate());
+    resetStateValues("operator", "firstOperand");
+    updateDisplayUI(calculatorState);
   }
   console.dir(calculatorState);
 });
+
+function setCalculateStateValue(property, value) {
+  calculatorState[property] = value;
+}
 
 function resetStateValues(...args) {
   const originalState = {
@@ -76,6 +61,10 @@ function resetStateValues(...args) {
   });
 }
 
+function updateDisplayUI({ displayValue }) {
+  display.innerText = displayValue;
+}
+
 clearBtn.addEventListener("click", () => {
   resetStateValues(
     "displayValue",
@@ -86,14 +75,15 @@ clearBtn.addEventListener("click", () => {
   display.innerText = calculatorState.displayValue;
 });
 
-function operate(operator, num1, num2) {
+function operate() {
+  const { displayValue, firstOperand, operator } = calculatorState;
   const operations = {
     "+": (num1, num2) => num1 + num2,
     "-": (num1, num2) => num1 - num2,
     "*": (num1, num2) => num1 * num2,
     "/": (num1, num2) => num1 / num2,
   };
-  return operations[operator](+num1, +num2);
+  return operations[operator](+firstOperand, +displayValue);
 }
 
 function updateDisplayValue(userValue) {
