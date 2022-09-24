135ca4bc1634a526acb97a41e2b01f82d6956a89
Melvin <melvin3ezell@gmail.com>
2022-09-23 22:49:46 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,95 @@
+"strict use";
+console.log("CONNECTED");
+const calculatorState = {
+  displayValue: "0",
+  firstOperand: null,
+  operator: null,
+  previousKeyType: null,
+};
+
+const calculator = document.querySelector(".calculator");
+const display = document.querySelector(".calculator h2");
+const clearBtn = document.querySelector("[data-type=clear]");
+
+calculator.addEventListener("click", (e) => {
+  const dataType = e.target.getAttribute("data-type");
+  // debugger;
+  if (dataType === "number") {
+    if (calculatorState.previousKeyType === "operator") {
+      calculatorState.firstOperand = calculatorState.displayValue;
+      calculatorState.displayValue = "0";
+    }
+    updateDisplayValue(e.target.innerText);
+    display.innerText = calculatorState.displayValue;
+    calculatorState.previousKeyType = "number";
+  }
+  if (dataType === "operator") {
+    if (
+      calculatorState.previousKeyType === "number" &&
+      calculatorState.firstOperand
+    ) {
+      // debugger;
+      calculatorState.displayValue = operate(
+        calculatorState.operator,
+        calculatorState.firstOperand,
+        calculatorState.displayValue
+      );
+      display.innerText = calculatorState.displayValue;
+    }
+    calculatorState.operator = e.target.innerText;
+    calculatorState.previousKeyType = "operator";
+  }
+  if (dataType === "calculate") {
+    if (calculatorState.displayValue && calculatorState.firstOperand) {
+      calculatorState.displayValue = operate(
+        calculatorState.operator,
+        calculatorState.firstOperand,
+        calculatorState.displayValue
+      );
+      calculatorState.operator = null;
+      calculatorState.firstOperand = null;
+      display.innerText = calculatorState.displayValue;
+    }
+  }
+  console.dir(calculatorState);
+});
+clearBtn.addEventListener("click", () => {
+  displayValue = "0";
+  display.innerText = displayValue;
+});
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+  const operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+  };
+  return operations[operator](+num1, +num2);
+}
+
+function updateDisplayValue(userValue) {
+  const { displayValue } = calculatorState;
+  if (displayValue === "0" || displayValue === null) {
+    calculatorState.displayValue = userValue;
+  } else {
+    calculatorState.displayValue += userValue;
+  }
+  return displayValue;
+}
